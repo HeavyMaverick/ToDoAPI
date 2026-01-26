4f0e318a3c42e0a863bd28d241c397530ab34637
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2026-01-26 16:09:07 +0700

    Update database.go
    Added logger

--- internal/database/database.go
+++ internal/database/database.go
@@ -5,9 +5,12 @@ import (
 	"ToDoApi/internal/model"
 	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func ConnectDB(cfg *config.Config) (*gorm.DB, error) {
@@ -15,10 +18,30 @@ func ConnectDB(cfg *config.Config) (*gorm.DB, error) {
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
 		cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort,
 	)
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags),
+		logger.Config{
+			SlowThreshold:             time.Second,
+			LogLevel:                  logger.Info,
+			IgnoreRecordNotFoundError: true,
+			Colorful:                  true,
+		},
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: newLogger,
+	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to database: %w", err)
 	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get generic database object: %w", err)
+	}
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
 	log.Println("Connected to PostgreSQL database")
 	return db, nil
 }
